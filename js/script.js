86c80451632d87328104a614dbbe05817758d012
Lucas Santos <85079606+eulsantoss@users.noreply.github.com>
2022-01-03 23:07:14 -0300

    Todos os conteúdos até o momento

---
+++ js/script.js
@@ -0,0 +1,743 @@
+const itemType = "RASTRO DE FUMAÇA"
+const itemName = "HENNA"
+const itemPresentation = "*url*"
+
+/*PAGE 1*/
+
+const henna = {
+    itemType: "RASTRO DE FUMAÇA",
+    itemName: "HENNA",
+    itemPresentation: "images/showitems/henna.png"
+}
+
+const shanta = {
+    itemType: "TRAJE",
+    itemName: "SHANTA",
+    itemPresentation: "images/showitems/shanta.png"
+}
+
+const ronin = {
+    itemType: "TRAJE",
+    itemName: "RONIN",
+    itemPresentation: "images/showitems/ronin.png"
+}
+
+const florAncestral = {
+    itemType: "ACESSÓRIO PARA AS COSTAS",
+    itemName: "FLOR ANCESTRAL",
+    itemPresentation: "images/showitems/flor-ancestral.png"
+}
+
+const desabrocharDaShanta = {
+    itemType: "SPRAY",
+    itemName: "DESABROCHAR DA SHANTA",
+    itemPresentation: "images/showitems/desabrochar-da-shanta.png"
+}
+
+const florEncantada = {
+    itemType: "ASA-DELTA",
+    itemName: "FLOR ENCANTADA",
+    itemPresentation: "images/showitems/flor-encantada.png"
+}
+
+const cimitarrasDeBronze = {
+    itemType: "FERRAMENTA DE COLETA",
+    itemName: "CIMITARRAS DE BRONZE",
+    itemPresentation: "images/showitems/cimitarras-de-bronze.png"
+}
+
+const moeda = {
+    itemType: "MOEDA",
+    itemName: "100 V-BUCKS",
+    itemPresentation: "images/showitems/v-buck.png"
+}
+
+const iconeDeEstandarte1 = {
+    itemType: "ÍCONE DE ESTANDARTE",
+    itemName: "ÍCONE DE ESTANDARTE",
+    itemPresentation: "images/showitems/icone-de-estandarte-1.png"
+}
+
+const vingancaDoRonin = {
+    itemType: "TELA DE CARREGAMENTO",
+    itemName: "VINGANÇA DO RONIN",
+    itemPresentation: "images/showitems/vinganca-do-ronin.png"
+}
+
+/*PAGE 2*/
+
+const recargaLunar = {
+    itemType: "GESTO",
+    itemName: "RECARGA LUNAR",
+    itemPresentation: ""
+}
+
+const crescenteAerea = {
+    itemType: "ASA-DELTA",
+    itemName: "CRESCENTE AÉREA",
+    itemPresentation: ""
+}
+
+const brasaoCarmesim = {
+    itemType: "ACESSÓRIO PARA AS COSTAS",
+    itemName: "BRASÃO CARMESIM",
+    itemPresentation: ""
+}
+
+const laminaDaLuaCrescente = {
+    itemType: "FERRAMENTA DE COLETA",
+    itemName: "LÂMINA DA LUA CRESCENTE",
+    itemPresentation: ""
+}
+
+const roninIndigo = {
+    itemType: "ESTILO",
+    itemName: "RONIN (ÍNDIGO)",
+    itemPresentation: ""
+}
+
+const lendaDaShanta = {
+    itemType: "TELA DE CARREGAMENTO",
+    itemName: "LENDA DA SHANTA",
+    itemPresentation: ""
+}
+
+const toqueDeLotus = {
+    itemType: "ENVELOPAMENTO",
+    itemName: "TOQUE DE LÓTUS",
+    itemPresentation: ""
+}
+
+const missaoDoRonin = {
+    itemType: "SPRAY",
+    itemName: "MISSÃO DO RONIN",
+    itemPresentation: ""
+}
+
+/*PAGE 3*/
+
+const luaCrescente = {
+    itemType: "ENVELOPAMENTO",
+    itemName: "LUA CRESCENTE",
+    itemPresentation: ""
+}
+
+const monstrinho = {
+    itemType: "GESTO",
+    itemName: "MONSTRINHO",
+    itemPresentation: ""
+}
+
+const arsenalLhamico = {
+    itemType: "ACESSÓRIO PARA AS COSTAS",
+    itemName: "ARSENAL LHÂMICO",
+    itemPresentation: ""
+}
+
+const chumboGrosso = {
+    itemType: "TELA DE CARREGAMENTO",
+    itemName: "CHUMBO GROSSO",
+    itemPresentation: ""
+}
+
+const florAncestralCapa = {
+    itemType: "ESTILO",
+    itemName: "FLOR ANCESTRAL (CAPA DA FLOR)",
+    itemPresentation: ""
+}
+
+const tenenteLauroLhama = {
+    itemType: "TRAJE",
+    itemName: "TENENTE LAURO LHAMA",
+    itemPresentation: ""
+}
+
+const chamaDoRonin = {
+    itemType: "EMOTICON",
+    itemName: "CHAMA DO RONIN",
+    itemPresentation: ""
+}
+
+const chamaCrescente = {
+    itemType: "RASTRO DE FUMAÇA",
+    itemName: "CHAMA CRESCENTE",
+    itemPresentation: ""
+}
+
+const iconeDeEstandarte2 = {
+    itemType: "ÍCONE DE ESTANDARTE",
+    itemName: "ÍCONE DE ESTANDARTE",
+    itemPresentation: ""
+}
+
+/*PAGE 4*/
+
+const lhamelhorDoQueNunca = {
+    itemType: "EMOTICON",
+    itemName: "LHAMELHOR DO QUE NUNCA",
+    itemPresentation: ""
+}
+
+const lhamaBelica = {
+    itemType: "ENVELOPAMENTO",
+    itemName: "LHAMA BÉLICA",
+    itemPresentation: ""
+}
+
+const lhaminador = {
+    itemType: "FERRAMENTA DE COLETA",
+    itemName: "LHAMINADOR",
+    itemPresentation: ""
+}
+
+const caminhaoLhamonstro = {
+    itemType: "ASA-DELTA",
+    itemName: "CAMINHÃO LHAMONSTRO",
+    itemPresentation: ""
+}
+
+const adorei = {
+    itemType: "GESTO",
+    itemName: "ADOREI",
+    itemPresentation: ""
+}
+
+const tenenteLauroLhamaForcasEspeciais = {
+    itemType: "ESTILO",
+    itemName: "TENENTE LAURO LHAMA (FORÇAS ESPECIAIS)",
+    itemPresentation: ""
+}
+
+const mordiscadaDaLhama = {
+    itemType: "SPRAY",
+    itemName: "MORDISCADA DA LHAMA",
+    itemPresentation: ""
+}
+
+const exploradores = {
+    itemType: "TELA DE CARREGAMENTO",
+    itemName: "EXPLORADORES",
+    itemPresentation: ""
+}
+
+/*PAGE 5*/
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+// const shanta = {
+//     itemType: "",
+//     itemName: "",
+//     itemPresentation: ""
+// }
+
+
+
+
+document.getElementById('card-1-a').addEventListener('click', ()=>{
+    document.getElementById('reward-area').innerHTML = 
+    `<img src="${henna.itemPresentation}" id="getResolution" alt="Representação do Rastro de Fumaça 'Henna'." />
+    <h4>${henna.itemType}</h4>
+    <h2>${henna.itemName}</h2>
+    <h3>ADQUIRIDO!</h3>`
+})
+
+document.getElementById('card-2-a').addEventListener('click', ()=>{
+    document.getElementById('reward-area').innerHTML = 
+    `<img src="${shanta.itemPresentation}" alt="Representação do Traje 'Shanta'." />
+    <h4>${shanta.itemType}</h3>
+    <h2>${shanta.itemName}</h2>
+    <h3>ADQUIRIDO!</h2>`
+})
+
+
+document.getElementById('card-3-a').addEventListener('click', ()=>{
+    document.getElementById('reward-area').innerHTML = 
+    `<img src="${ronin.itemPresentation}" alt="Representação do Traje 'Shanta'." />
+    <h4>${ronin.itemType}</h3>
+    <h2>${ronin.itemName}</h2>
+    <h3>ADQUIRIDO!</h2>`
+})
+
+document.getElementById('card-4-a').addEventListener('click', ()=>{
+    document.getElementById('reward-area').innerHTML = 
+    `<img src="${florAncestral.itemPresentation}" alt="Representação do Traje 'Shanta'." />
+    <h4>${florAncestral.itemType}</h3>
+    <h2>${florAncestral.itemName}</h2>
+    <h3>ADQUIRIDO!</h2>`
+})
+
+document.getElementById('card-5-a').addEventListener('click', ()=>{
+    document.getElementById('reward-area').innerHTML = 
+    `<img src="${desabrocharDaShanta.itemPresentation}" alt="Representação do Traje 'Shanta'." />
+    <h4>${desabrocharDaShanta.itemType}</h3>
+    <h2>${desabrocharDaShanta.itemName}</h2>
+    <h3>ADQUIRIDO!</h2>`
+})
+
+document.getElementById('card-6-a').addEventListener('click', ()=>{
+    document.getElementById('reward-area').innerHTML = 
+    `<img src="${florEncantada.itemPresentation}" alt="Representação do Traje 'Shanta'." />
+    <h4>${florEncantada.itemType}</h3>
+    <h2>${florEncantada.itemName}</h2>
+    <h3>ADQUIRIDO!</h2>`
+})
+
+document.getElementById('card-7-a').addEventListener('click', ()=>{
+    document.getElementById('reward-area').innerHTML = 
+    `<img src="${cimitarrasDeBronze.itemPresentation}" alt="Representação do Traje 'Shanta'." />
+    <h4>${cimitarrasDeBronze.itemType}</h3>
+    <h2>${cimitarrasDeBronze.itemName}</h2>
+    <h3>ADQUIRIDO!</h2>`
+})
+
+document.getElementById('card-8-a').addEventListener('click', ()=>{
+    document.getElementById('reward-area').innerHTML = 
+    `<img src="${moeda.itemPresentation}" alt="Representação do Traje 'Shanta'." />
+    <h4>${moeda.itemType}</h3>
+    <h2>${moeda.itemName}</h2>
+    <h3>ADQUIRIDO!</h2>`
+})
+
+document.getElementById('card-9-a').addEventListener('click', ()=>{
+    document.getElementById('reward-area').innerHTML = 
+    `<img src="${iconeDeEstandarte1.itemPresentation}" alt="Representação do Traje 'Shanta'." />
+    <h4>${iconeDeEstandarte1.itemType}</h3>
+    <h2>${iconeDeEstandarte1.itemName}</h2>
+    <h3>ADQUIRIDO!</h2>`
+})
+
+document.getElementById('card-10-a').addEventListener('click', ()=>{
+    document.getElementById('reward-area').innerHTML = 
+    `<img src="${vingancaDoRonin.itemPresentation}" alt="Representação do Traje 'Shanta'." />
+    <h4>${vingancaDoRonin.itemType}</h3>
+    <h2>${vingancaDoRonin.itemName}</h2>
+    <h3>ADQUIRIDO!</h2><br>
+    <button id="fullscreenButton">TELA CHEIA</button>`
+    document.getElementById('fullscreenButton').addEventListener('click', ()=>{ //Função disparada ao clicar no "TELA CHEIA"
+        const divFullscreen = document.getElementById('imageOpenned') //Div pai onde ficará a imagem
+        const fullscreenImage = document.createElement('img') //Imagem que será criada/carregada após clicar em "TELA CHEIA"
+        fullscreenImage.setAttribute('id', 'imageClose') //Recuperando a variável constante que representa a imagem e atribuindo ao elemento um ID
+        fullscreenImage.src = 'images/showitems/vinganca-do-ronin-fs.png' //Determinando qual será o src (caminho) da imagem que será carregada
+        divFullscreen.appendChild(fullscreenImage) //Determinando que a variável "fullscreenImage" será o elemento (div) filho da variável (div) "divFullscreen"
+        document.body.style = 'overflow: hidden;' //Juntamente com o carregamento da imagem, a rolagem do site será desativada
+        divFullscreen.innerHTML += 
+        '<p id="closeImage">X</p>' //Criação de um parágrafo dentro da "divFullscreen" que servirá como botão de fechar a imagem que foi aberta
+        document.getElementById('closeImage').addEventListener('click', ()=>{ //Criação da função de fechar a imagem e atribuição desta para o botão de fechar
+            document.getElementById('imageClose').remove() //Remove este elemento
+            document.getElementById('closeImage').remove() //Remove este elemento
+            document.body.style = "overflow: auto;" //Reativa a rolagem do site
+        })
+    })
+    
+})
+
+//PAGE 2
+
+document.getElementById('card-1-b').addEventListener('click', ()=>{
+    document.getElementById('reward-area').innerHTML = 
+    `<img src="${moeda.itemPresentation}" id="getResolution" alt="Representação do Rastro de Fumaça ''." />
+    <h4>${moeda.itemType}</h4>
+    <h2>${moeda.itemName}</h2>
+    <h3>ADQUIRIDO!</h3>`
+})
+
+
+
+
+
+
+document.getElementById('card-6-b').addEventListener('click', ()=>{
+    document.getElementById('reward-area').innerHTML = 
+    `<img src="${moeda.itemPresentation}" id="getResolution" alt="Representação do Rastro de Fumaça ''." />
+    <h4>${moeda.itemType}</h4>
+    <h2>${moeda.itemName}</h2>
+    <h3>ADQUIRIDO!</h3>`
+})
+
+
+
+//PAGE 3
+
+
+
+
+document.getElementById('card-7-c').addEventListener('click', ()=>{
+    document.getElementById('reward-area').innerHTML = 
+    `<img src="${moeda.itemPresentation}" id="getResolution" alt="Representação do Rastro de Fumaça ''." />
+    <h4>${moeda.itemType}</h4>
+    <h2>${moeda.itemName}</h2>
+    <h3>ADQUIRIDO!</h3>`
+})
+
+//PAGE 4
+
+document.getElementById('card-7-d').addEventListener('click', ()=>{
+    document.getElementById('reward-area').innerHTML = 
+    `<img src="${moeda.itemPresentation}" id="getResolution" alt="Representação do Rastro de Fumaça ''." />
+    <h4>${moeda.itemType}</h4>
+    <h2>${moeda.itemName}</h2>
+    <h3>ADQUIRIDO!</h3>`
+})
+
+
+
+
+document.getElementById('card-10-d').addEventListener('click', ()=>{
+    document.getElementById('reward-area').innerHTML = 
+    `<img src="${moeda.itemPresentation}" id="getResolution" alt="Representação do Rastro de Fumaça ''." />
+    <h4>${moeda.itemType}</h4>
+    <h2>${moeda.itemName}</h2>
+    <h3>ADQUIRIDO!</h3>`
+})
